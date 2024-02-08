7a73bb5e18ac91501d7908bb72883d5adf6b8695
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2024-02-08 12:59:40 -0600

    edited wheatherQueries.py

--- WheatherData/wheatherQueries.py
+++ WheatherData/wheatherQueries.py
@@ -18,7 +18,7 @@ def test_connection():
 
     return None
 
-def get_everything_from(table): #Works well
+def get_everything_from(table): #Works well - tested with all tables and no errors found with the tables - Noah
     try:
         conn = psycopg2.connect(
         host="localhost",
@@ -49,10 +49,45 @@ def get_everything_from(table): #Works well
         if conn is not None:
             conn.close()
 
+def get_country_temp(country): #by Noah
+    try:
+        conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="dawsonj2",
+        user="dawsonj2",
+        password="eyebrow529redm")
+
+        cur = conn.cursor()
+
+        sql = """
+        SELECT temperature_table.tempc, weather_r.last_updated 
+        FROM temperature_table 
+        JOIN weather_r ON temperature_table.instance_id = weather_r.instance_id 
+        JOIN country ON weather_r.country = country.country 
+        WHERE country.country = %s;
+        """
+        
+        cur.execute(sql, country)
+        rows = cur.fetchall()
+
+        if(rows is not None):
+            for row in rows:
+                print(row)
+        else:
+            print("No instance found for ", country)
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
 
 def main():
     test_connection()
-    get_everything_from("sunmoon")
+    get_country_temp("Argentina")
+
 
 
 main()
