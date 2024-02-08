d0de89dbbe4869e77acf4afdf30c9cf904cdbc80
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2024-02-08 13:04:43 -0600

    edited wheatherQueries.py

--- WheatherData/wheatherQueries.py
+++ WheatherData/wheatherQueries.py
@@ -49,7 +49,7 @@ def get_everything_from(table): #Works well - tested with all tables and no erro
         if conn is not None:
             conn.close()
 
-def get_country_temp(country): #by Noah
+def get_country_temps(country): #by Noah - Works well!
     try:
         conn = psycopg2.connect(
         host="localhost",
@@ -84,9 +84,47 @@ def get_country_temp(country): #by Noah
         if conn is not None:
             conn.close()
 
+def get_highest_temp(country): #by Noah 
+    try:
+        conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="dawsonj2",
+        user="dawsonj2",
+        password="eyebrow529redm")
+
+        cur = conn.cursor()
+
+        sql = """
+        SELECT TOP 3 temperature_table.tempc, weather_r.last_updated 
+        FROM temperature_table 
+        JOIN weather_r ON temperature_table.instance_id = weather_r.instance_id 
+        JOIN country ON weather_r.country = country.country
+        ORDER BY temperature_table.tempc DESC  
+        WHERE country.country = %s;
+        """
+        
+        cur.execute(sql, (country,))
+        rows = cur.fetchall()
+
+        if(rows is not None):
+            for row in rows:
+                print(row)
+        else:
+            print("No instance found for ", country)
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    
+
 def main():
     test_connection()
-    get_country_temp("Malaysia")
+    get_highest_temp("China")
 
 
 
