2d4a1f1c7720ce96367e1c3d67b8f56f6807daf5
Warren Kozak <warren.john.kozak@gmail.com>
2024-02-25 19:11:27 -0500

    Code structuring changes

--- FrontEndWheather/app.py
+++ FrontEndWheather/app.py
@@ -29,53 +29,76 @@ cur = conn.cursor()
 def index():
     return redirect('/login')
 
-@app.route('/login')
-def login_page():
-    return render_template("login.html")
-
-@app.route('/register')
-def register_page():
-    return render_template("register.html")
-
 @app.route('/user/<username>')
 def home(username):
     return render_template("homepage.html", username = username)
 
-@app.route('/submit', methods=['POST'])
-def login_post():
-    user_username = request.form['username']
-    user_password = request.form['password']
-    
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    # If the method is POST
     if request.method == 'POST':
-        if 'login' in request.form:
-            cur.execute("SELECT password,salt FROM users WHERE login='{0}';".format(user_username))
-            pass_info = cur.fetchone()
+        user_username = request.form['username']
+        user_password = request.form['password']
+        user_email = request.form['email']
+        user_confirm_password = request.form['confirmPassword']
 
-            if pass_info is None:
-                return render_template("login.html", incorrect_pass = True)
-            else:
-                correct_pass, salt = pass_info
+        # Check if the user already exists
+        cur.execute("SELECT login FROM users WHERE login='{0}';".format(user_username))
+        if cur.fetchone() is not None:
+            return render_template("register.html", invalid = True, error_text = "User already exists. Please try again.")
 
-            if hash(user_password, salt) == correct_pass:
-                return redirect(url_for('home', username = user_username))
-            else:
-                return render_template("login.html", incorrect_pass = True)
+        # Check if the passwords match
+        if user_password != user_confirm_password:
+            return render_template("register.html", invalid = True, error_text = "Passwords do not match. Try again.")
 
-        elif 'register' in request.form:
-            cur.execute("SELECT login FROM users WHERE login='{0}';".format(user_username))
-            if cur.fetchone() is not None:
-                return render_template("login.html", loginText = "That username is already taken. Try again.")
+        # Check if the username or password is blank
+        if user_username == "" or user_password == "":
+            return render_template("register.html", invalid = True, error_text = "Please enter a username and password.")
 
-            if user_username == "" or user_password == "":
-                return render_template("login.html", loginText = "Please enter a username and password.")              
-            salt = binascii.hexlify(os.urandom(16)).decode()
-            cur.execute("INSERT INTO users VALUES ('" + user_username + "', '" + hash(user_password, salt) + "', '" + salt + "')")
-            conn.commit()
-            return render_template("registered.html", username = user_username)
+        # If no errors, return the template
+        salt = gen_salt(16)
+        cur.execute("INSERT INTO users VALUES ('{0}', '{1}', '{2}')".format(user_username, hash(user_password, salt), salt))
+        conn.commit()
+        return render_template("registered.html", username = user_username)
 
+    # If the method is GET
+    return render_template("register.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    # If the method is POST
+    if request.method == 'POST':
+        user_username = request.form['username']
+        user_password = request.form['password']
+
+        # Request the password and salt from the database
+        cur.execute("SELECT password,salt FROM users WHERE login='{0}';".format(user_username))
+        pass_info = cur.fetchone()
+
+        # Check to see if the user exists in the database
+        if pass_info is None:
+            return render_template("login.html", incorrect_pass = True)
+        correct_pass, salt = pass_info
+
+        # Check to see if the user entered password matches the database entry
+        if hash(user_password, salt) == correct_pass:
+            return redirect(url_for('home', username = user_username))
+        return render_template("login.html", incorrect_pass = True)
+
+    # If the method is POST
+    return render_template("login.html")
+
+# Request a query from the postgres database
 def query_result(query):
     pass
 
+# Generate a random salt
+def gen_salt(size):
+    return binascii.hexlify(os.urandom(size)).decode()
+
+# Hash the password and the salt
 def hash(password, salt):
     # Initialize the sha256 instance
     sha256 = hashlib.sha256()
