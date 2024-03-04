09a21b8f6a47bb6cadf1e9382249606f45271345
Noah Lee <leen2@carleton.edu>
2024-03-04 17:28:11 -0600

    edited app.py and added a new js file

--- FrontEndWheather/static/calendarScript.js
+++ FrontEndWheather/static/calendarScript.js
@@ -2,28 +2,16 @@
 
 $('#calendar').on('change', function() {
     var selectedDate = $(this).val();
-    var selectedCountry = $('#secret-country').val();
+    var selectedCountry = $('#secret-country').text();
     $.getJSON('/get-map-data', {'country':selectedCountry, 'date':selectedDate}, function(data){ //goes to app.py
         console.log(data);
         if(!data.error){
-            var temp = data.temp;
-            var wind = data.wind;
-            var precip = data.precip;
-            var sunrise = data.sunrise;
-            var sunset = data.sunset;
-            var moonphase = data.moonphase;
-            $('#temp-val').empty();
-            $('#wind-val').empty();
-            $('#precip-val').empty();
-            $('#sunrise-val').empty();
-            $('#sunset-val').empty();
-            $('#moonphase-val').empty();
-            $('#temp-val').val(temp);
-            $('#wind-val').val(wind);
-            $('#precip-val').val(precip);
-            $('#sunrise-val').val(sunrise);
-            $('#sunset-val').val(sunset);
-            $('#moon-val').val(moonphase);
+            $('#temp-val').text(data.temp + ' °C');
+            $('#wind-val').text(data.wind + ' km/h');
+            $('#precip-val').text(data.precip + ' mm');
+            $('#sunrise-val').text(data.sunrise);
+            $('#sunset-val').text(data.sunset);
+            $('#moon-val').text(data.moonphase);
         }else{
             console.log("error2");
         }
