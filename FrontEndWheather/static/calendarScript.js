659c1d96afbee3618e41dba6f6cb49c5d82ebc3b
Noah Lee <leen2@carleton.edu>
2024-03-04 21:23:19 -0600

    edited app.py and added a new js file

--- FrontEndWheather/static/calendarScript.js
+++ FrontEndWheather/static/calendarScript.js
@@ -3,6 +3,8 @@
 $('#calendar').on('change', function() {
     var selectedDate = $(this).val();
     var selectedCountry = $('#secret-country').text();
+    console.log(selectedDate);
+    console.log(selectedCountry);
     $.getJSON('/get-map-data', {'country':selectedCountry, 'date':selectedDate}, function(data){ //goes to app.py
         console.log(data);
         if(!data.error){
